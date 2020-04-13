9710b138eb092b9db9661f32a28bf145b271a3b9
fernando costa <fernando.costa@iviatecnologia.com.br>
2020-04-13 11:32:31 -0300

    Adicionado projeto

--- src/app/components/view-registration/view-registration.component.ts
+++ src/app/components/view-registration/view-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BikeService} from '../../services/bike.service';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-view-registration',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewRegistrationComponent implements OnInit {
 
-  constructor() { }
+  public bikeReg;
+  constructor(private bikeService: BikeService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getbikeReg(this.route.snapshot.params.id);
+  }
+
+  getbikeReg(id: number){
+    this.bikeService.getBike(id).subscribe(
+      data => {
+        this.bikeReg = data;
+      },
+      err => console.error(err),
+      () => console.log('bike loaded')
+    );
   }
 
 }
--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+//import { Observable } from 'rxjs/Observable';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Types': 'application/json' })
@@ -9,9 +9,18 @@ const httpOptions = {
 @Injectable()
 export class BikeService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getBikes(){
     return this.http.get('/server/api/v1/bikes');
+  }
+
+  getBike(id: number){
+    return this.http.get('/server/api/v1/bikes/' + id);
+  }
+
+  createBikeRegistration(bike){
+    const body = JSON.stringify(bike);
+    return this.http.post('/server/api/v1/bikes', body, httpOptions);
   }
 }
